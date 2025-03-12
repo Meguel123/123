cbe73d518784ac7ddd44b76abb4f95dcaec0c638
Miguel P <miguel.perassoli@gmail>
2025-03-12 14:06:31 -0300

    musica

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
-﻿Console.WriteLine(@"
-Miguel P
-Guilçherme
-");
+﻿Console.Clear();
+Console.Beep(440, 500); //lá
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write(@" _  
+/  | 
+`| | 
+ | | 
+_| |_
+\___/");
+Thread.Sleep(2000);
+Console.Clear();
+Console.Beep(440, 500); //lá
+Console.Write(@"/ __  \
+`' / /'
+  / /  
+./ /___
+\_____/");
+Thread.Sleep(800);
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear();
+Console.Beep(440, 500);
+Console.Write(@" _____ 
+|____ |
+    / /
+    \ \
+.___/ /
+\____/ "); //lá
+Thread.Sleep(500);
+Console.Clear();
+Console.Beep(640, 1000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write(@" _____ _____ _ 
+|  __ \  _  | |
+| |  \/ | | | |
+| | __| | | | |
+| |_\ \ \_/ /_|
+ \____/\___/(_)"); //si#
+ Console.ResetColor();
